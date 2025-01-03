717106a380592d98cf901dead94c930b6bb7c09c
NoticeTrades <nickthomasfx@gmail.com>
2025-01-03 01:49:21 -0500

    Add exercises and notes for Chapter 1: Variables and Data Types

---
+++ chapter_1_basics/chapter_1_code.py
@@ -0,0 +1,27 @@
+#Exercise 1: Declaring and Using Variables
+
+"""Create a variable called name and assign it your name
+"""
+
+"""Create another variable age and assign it your age"""
+
+"""Add a variable is_trader and set it to True"""
+
+"""print a sentence that says: "Hi, my name is [name]. 
+I am [age] years old, and it's [is_trader] that I am trader"""
+"Use an f-string for this"
+
+name = 'nick'
+age = 23
+is_trader = True
+
+print(f"Hi, my name is {name}. I am {age} years old, and it's {is_trader} that I am a trader.")
+
+#Exercise 2: Playing with Data Types
+
+"""Create three variables: shares (integer), price_per_share(float), and ticker (string, e.g., "NVDA")"""
+
+"""Calculate the total cost of buying the shares and store it in a variable called total_cost"""
+
+"""Print a sentence that shows the ticker, number of shares, and total cost"""
+
