65dc577149c1b6fe5c258fb581455db28a019329
NoticeTrades <nickthomasfx@gmail.com>
2025-01-03 06:44:34 -0500

    Add Chapter 2 code and notes on control structures

--- chapter_1_basics/chapter_1_code.py
+++ chapter_1_basics/chapter_1_code.py
@@ -25,3 +25,39 @@ print(f"Hi, my name is {name}. I am {age} years old, and it's {is_trader} that I
 
 """Print a sentence that shows the ticker, number of shares, and total cost"""
 
+shares = 10
+price_per_share = 42.30
+ticker = "NVDA"
+
+total_cost = shares * price_per_share
+
+print(f"I bought {shares} shares of {ticker} at a price of {price_per_share:.2f} per share for a total cost of {total_cost:.2f}")
+
+#Exercise 3: Calculating the Average PPS (price per share)
+
+""""" Youre buying shares of a stock in multiple transactions. Write a program to calculate 
+the average price per share based on the total cost and the total number of shares purchased.
+"""
+
+# Defining shares and prices for three transactions
+shares_1 = 10
+price_per_share_1 = 30.42
+shares_2 = 15
+price_per_share_2 = 52.32
+shares_3 = 42
+price_per_share_3 = 28.32
+
+# Calculating the total cost for each transaction
+total_cost_1 = shares_1 * price_per_share_1
+total_cost_2 = shares_2 * price_per_share_2
+total_cost_3 = shares_3 * price_per_share_3
+
+# Overall total cost and total number of shares
+overall_total = total_cost_1 + total_cost_2 + total_cost_3
+total_shares = shares_1 + shares_2 + shares_3
+
+# Calculating the average price per share
+average_pps = overall_total / total_shares
+
+# Displaying the result
+print(f"The average price per share for the stock is ${average_pps:.2f}")
